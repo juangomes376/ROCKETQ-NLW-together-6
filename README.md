b0ee9f3bd6217760dccc2495710a82b8bce66908
Juan Gomes <juangomes376@gmail.com>
2022-02-14 01:33:10 +0100

    update

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto desenvolvido na NLW Together Mission: Discover da ROCKETSEAT
--- src/route.js
+++ src/route.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const req = require('express/lib/request')
+const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
-route.get('/room', (req, res) => res.render("room"))
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/', (req, res) => res.render("index", { page: 'enter-room' }))
+route.get('/create-pass', (req, res) => res.render("index", { page: 'create-pass' }))
 
-route.get('/room/:room/:question/:action', (req, res) => res.render("exemple", { req }))
+route.get('/room/:room', (req, res) => res.render("room"))
+
+//formato que o formulario de dentro da modal tem que passar as informacoes
+route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/create-room', RoomController.create)
 
 module.exports = route
